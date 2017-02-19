0c1f0543e21ed8a3ac3b8d4238c91a2dda47f05d
leon66666 <1902518544@qq.com>
2017-02-19 21:18:28 +0800

    modify ehache

--- src/main/java/zhongqiu/ehcache/EhcacheDemo.java
+++ src/main/java/zhongqiu/ehcache/EhcacheDemo.java
@@ -1,5 +1,8 @@
 package zhongqiu.ehcache;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.ehcache.*;
 
 //参考：http://www.cnblogs.com/jingmoxukong/p/5975994.html
@@ -15,15 +18,29 @@ public class EhcacheDemo {
 		final String key = "greeting";
 
 		// Create a data element
-		final Element putGreeting = new Element(key, "Hello, World!");
+		Element putGreeting = new Element(key, "Hello, World!");
 
 		// Put the element into the data store
 		testCache.put(putGreeting);
 
 		// Retrieve the data element
-		final Element getGreeting = testCache.get(key);
+		Element getGreeting = testCache.get(key);
 
 		// Print the value
 		System.out.println(getGreeting.getObjectValue());
+
+		// 集合
+		String listKey = "list";
+		ArrayList<String> aList = new ArrayList<>();
+		aList.add("a");
+		aList.add("b");
+		aList.add("c");
+		putGreeting = new Element(listKey, aList);
+		testCache.put(putGreeting);
+		getGreeting = testCache.get(listKey);
+		aList = (ArrayList<String>) getGreeting.getObjectValue();
+		for (String string : aList) {
+			System.out.println(string);
+		}
 	}
 }
